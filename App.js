c4ca9742489a19949d74c2b5ba4f9139ff579e42
jaehun <wognskec@hanyang.ac.kr>
2022-02-03 23:26:15 +0900

    first react-native

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import AppLoading from "expo-app-loading";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [ready, setReady] = useState(false);
+  // ! 1st
+  const startAsync = () => async () => {
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+  };
+  // ! 2nd
+  const onFinish = () => setReady(true);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  if (!ready) {
+    return (
+      <AppLoading
+        startAsync={startAsync}
+        onFinish={onFinish}
+        onError={console.error}
+      />
+    );
+  }
+  // !3rd
+  return null;
+}
